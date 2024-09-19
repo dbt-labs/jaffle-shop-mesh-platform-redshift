b80b18a0c61e3216cd102e38fa6ab456b0a0868b
dave.connors@fishtownanalytics.com <dave.connors@fishtownanalytics.com>
2024-09-19 15:35:28 +0000

    please?

---
+++ models/marts/orders.sql
@@ -0,0 +1,51 @@
+with
+
+jaffle_shop_orders as (
+    
+    select * from {{ ref('jaffle_shop_orders') }}
+    
+),
+
+circus_concessions as (
+    
+    select * from {{ ref('cirque_du_jaffle', 'concession_sales') }}
+
+),
+
+unioned as (
+
+    select 
+        'jaffle_shop' as sale_location,
+        cast(order_id as text) as order_id,
+        coalesce(count_food_items, 0) as total_jaffles_purchased,
+        coalesce(food_order_items_subtotal, 0) as total_jaffle_revenue
+    
+    from jaffle_shop_orders
+
+    union all 
+
+    select 
+        'cirque_du_jaffle' as sale_location,
+        cast(concession_sale_id as text) as order_id,
+        coalesce(total_jaffles_purchased, 0) as total_jaffles_purchased,
+        coalesce(order_jaffle_subtotal, 0) as total_jaffle_revenue
+    
+    from circus_concessions
+
+),
+
+final as (
+
+    select 
+        {{
+            dbt_utils.generate_surrogate_key([
+                'order_id',
+                'sale_location'
+            ])
+        }} as unique_key,
+        unioned.*
+    from unioned
+
+)
+
+select * from final
